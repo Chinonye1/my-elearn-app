64144f88162615110a79f08fecd25a1ab8212d06
Chinonye <faithchinonye53@gmailcom>
2026-06-10 12:03:53 +0200

    feat: add instructor dashboard with course stats and table

--- src/pages/Instructor.jsx
+++ src/pages/Instructor.jsx
@@ -10,6 +10,18 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
+function createData(courseTitle, category, students, revenues, rating) {
+  return {courseTitle, category, students, revenues, rating };
+}
+
+const rows = [
+  createData('Complete React Development Course', "Programming", 1.235, "$10500", 4.0),
+  createData('Advanced JavaScript Mastery', "Programming", 856, "$7500", 4.3),
+  createData('Python for Data Science', "Date Science", 2.103, "$4500", 6.0),
+  createData('UI/UX Design Fundamentals', "Design",456 , "$2500", 4.3),
+  createData('Machine Learning', "Machine", 321, "$3500", 3.9),
+];
+
 export function Instructor() {
     const navigate = useNavigate();
 
@@ -67,6 +79,36 @@ export function Instructor() {
 
 
         </div>
+
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell> Course Title </TableCell>
+            <TableCell align="right">Category</TableCell>
+            <TableCell align="right">Student</TableCell>
+            <TableCell align="right">Revenue&nbsp;($)</TableCell>
+            <TableCell align="right">Rating</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.courseTitle}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.courseTitle}
+              </TableCell>
+              <TableCell align="right">{row.category}</TableCell>
+              <TableCell align="right">{row.students}</TableCell>
+              <TableCell align="right">{row.revenues}</TableCell>
+              <TableCell align="right">{row.rating}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </>
     )
     
